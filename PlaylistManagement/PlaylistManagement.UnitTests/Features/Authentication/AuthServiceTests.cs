ea58da193cabca954a284624073f3edfa68ab4a4
Mariz Youssef <mariz.youssef7@gmail.com>
2026-09-03 21:44:54 +0300

    test: add playlist service unit tests

---
+++ PlaylistManagement/PlaylistManagement.UnitTests/Features/Authentication/AuthServiceTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaylistManagement.UnitTests.Features.Authentication
+{
+    internal class AuthServiceTests
+    {
+    }
+}
